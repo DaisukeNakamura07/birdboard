919f72fe24e0c0ff9b624956187a5dbc2dc46d07
中村大輔 <nakamuratakeshi@nakamura-no-Mac-mini.local>
2019-03-30 08:19:41 +0900

    EP26 complete

--- app/Project.php
+++ app/Project.php
@@ -37,18 +37,26 @@ class Project extends Model
     public function recordActivity($description)
     {
         // var_dump(
-        //     array_diff($this->old, $this->toArray()
+        //     array_diff($this->old, $this->toArray()//toArray()で$project
         // ));
         $this->activity()->create([
             'description' => $description,
-            'changes' => [
-                'before' => array_diff($this->old, $this->getAttributes()),//array_diff(array1, array2) これでarray1, array2の中身を比較し、array1にだけ存在するものを返す。
-                'after' => array_diff($this->getAttributes(), $this->old)//afterには変更点だけ保存。
-            ]
-        ]); //create(compact('description')) means create(['description' => $description])
+            'changes' => $this->activityChanges($description)
+        ]);
+        //create(compact('description')) means create(['description' => $description])
        //  Activity::create([
        //      'project_id' => $this->id,
        //      'description' => $type
        // ]);
+    }
+
+    protected function activityChanges($description)
+    {
+        if($description == 'updated') {
+            return [
+                'before' => array_except(array_diff($this->old, $this->getAttributes()), 'updated_at'), //array_diff(array1, array2) これでarray1,array2の中身を比較し、array1にだけ存在するものを返す。getAttributes()で$projectのattributesを配列で返す。
+                'after' => array_except($this->getChanges(), 'updated_at')//afterには変更点だけ保存。getChanges()で変更点を返す。array_except(配列,配列の除外キー)で'updated_at'以外のデータを返す。
+            ];   
+        }
     }
 }
