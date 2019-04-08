63ecad742821d975e1d0a36002a0f0b40e5ff629
中村大輔 <nakamuratakeshi@nakamura-no-Mac-mini.local>
2019-04-08 13:22:22 +0900

    comment out observer files

--- app/Observers/ProjectObserver.php
+++ app/Observers/ProjectObserver.php
@@ -1,68 +1,68 @@
 <?php
 
-namespace App\Observers;
+// namespace App\Observers;
 
-use App\Project;
+// use App\Project;
 
-class ProjectObserver
-{
-    /**
-     * Handle the project "created" event.
-     *
-     * @param  \App\Project  $project
-     * @return void
-     */
-    public function created(Project $project)
-    {
-        $project->recordActivity('created');
-    }
+// class ProjectObserver
+// {
+//     /**
+//      * Handle the project "created" event.
+//      *
+//      * @param  \App\Project  $project
+//      * @return void
+//      */
+//     public function created(Project $project)
+//     {
+//         $project->recordActivity('created');
+//     }
 
-    public function updating(Project $project)
-    {
-        $project->old = $project->getOriginal();
-    }
+//     // public function updating(Project $project)//traitへ抽出
+//     // {
+//     //     $project->old = $project->getOriginal();
+//     // }
 
-    /**
-     * Handle the project "updated" event.
-     *
-     * @param  \App\Project  $project
-     * @return void
-     */
-    public function updated(Project $project)
-    {
-        $project->recordActivity('updated');
-    }
+//     /**
+//      * Handle the project "updated" event.
+//      *
+//      * @param  \App\Project  $project
+//      * @return void
+//      */
+//     public function updated(Project $project)
+//     {
+//         $project->recordActivity('updated');
+//     }
 
-    /**
-     * Handle the project "deleted" event.
-     *
-     * @param  \App\Project  $project
-     * @return void
-     */
-    public function deleted(Project $project)
-    {
-        //
-    }
+//     /**
+//      * Handle the project "deleted" event.
+//      *
+//      * @param  \App\Project  $project
+//      * @return void
+//      */
+//     public function deleted(Project $project)
+//     {
+//         //
+//     }
 
-    /**
-     * Handle the project "restored" event.
-     *
-     * @param  \App\Project  $project
-     * @return void
-     */
-    public function restored(Project $project)
-    {
-        //
-    }
+//     /**
+//      * Handle the project "restored" event.
+//      *
+//      * @param  \App\Project  $project
+//      * @return void
+//      */
+//     public function restored(Project $project)
+//     {
+//         //
+//     }
 
-    /**
-     * Handle the project "force deleted" event.
-     *
-     * @param  \App\Project  $project
-     * @return void
-     */
-    public function forceDeleted(Project $project)
-    {
-        //
-    }
-}
+//     /**
+//      * Handle the project "force deleted" event.
+//      *
+//      * @param  \App\Project  $project
+//      * @return void
+//      */
+//     public function forceDeleted(Project $project)
+//     {
+//         //
+//    }
+// }
